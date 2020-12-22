c2a87ed40033b255f34294771a13d9590b58791e
cristian alvarado esperano <Cris_ales@LIVE.COM.MX>
2020-12-22 16:48:27 -0700

    Se agregan datos al modelo

--- Models/Dosificador/DatosNotaRemision.cs
+++ Models/Dosificador/DatosNotaRemision.cs
@@ -15,6 +15,7 @@ namespace Models.Dosificador
         public string Estatus { get; set; }
         public string Obra { get; set; }
         public string HoraSalida { get; set; }
+        public string Formula { get; set; }
         public string Producto { get; set; }
         public string Operador { get; set; }
         public string Equipo { get; set; }
@@ -22,13 +23,13 @@ namespace Models.Dosificador
         public string BombaEquipo { get; set; }
         public DateTime Fecha { get; set; }
         public string FechaConFormato { get; set; }
+        public string CodOperador_1 { get; set; }
+        public string CodOperador_2 { get; set; }
+        public string CodEquipo_CR { get; set; }
+        public string CodEquipo_BB { get; set; }
         public bool Bombeable { get; set; }
         public bool Fibra { get; set; }
         public bool Imper { get; set; }
         public double Cantidad { get; set; }
-
-
-
-
     }
 }
