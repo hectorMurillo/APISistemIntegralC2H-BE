f3790cc199a687fb39e8abf0cb6a0334c2edf807
ChristianAlvarado <cris_ales@live.com.mx>
2020-12-24 14:27:22 -0700

    Modificacion al obtener la nota de remision del agente

--- Models/Dosificador/OperadorModel.cs
+++ Models/Dosificador/OperadorModel.cs
@@ -9,10 +9,11 @@ namespace Models.Dosificador
     public class OperadorModel
     {
         public int Codigo { get; set; }
-        public string nombreCompleto { get; set; }
+        public string  NombreCompleto { get; set; }
 		public string RFC { get; set; }
-
-		public int CodigoTipoEmpleado { get; set; }
+        public int CodEquipoAsignado { get; set; }
+        public string Equipo { get; set; }
+        public int CodigoTipoEmpleado { get; set; }
 		public bool Estatus { get; set; }
 		public string Correo { get; set; }
 		public string Telefono { get; set; }
