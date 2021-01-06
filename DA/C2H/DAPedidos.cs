17e0c5f711c6add0563925be7e25e5c2b87a00c2
mario sanz <josesl@difarmer.com>
2021-01-05 17:59:21 -0700

    subiendo primeros cambios para los pedidos.

---
+++ DA/C2H/DAPedidos.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using Models.Pedidos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarmPack.Classes;
+using WarmPack.Database;
+
+namespace DA.C2H
+{
+    public class DAPedidos
+    {
+        private readonly Conexion _conexion = null;
+
+        public DAPedidos()
+        {
+            _conexion = new Conexion(ConexionType.MSSQLServer, Globales.ConexionPrincipal);
+        }
+
+        public Result<List<Pedido>> ObtenerPedidos(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            Result<List<Pedido>> result = new Result<List<Pedido>>();
+            try
+            {
+                var parametros = new ConexionParameters();
+                parametros.Add("@pFechaDesde", ConexionDbType.Date, fechaDesde);
+                parametros.Add("@pFechaHasta", ConexionDbType.Date, fechaHasta);
+                parametros.Add("@pResultado", ConexionDbType.Bit, System.Data.ParameterDirection.Output);
+                parametros.Add("@pMsg", ConexionDbType.VarChar, System.Data.ParameterDirection.Output, 300);
+
+                result = _conexion.ExecuteWithResults<Pedido>("ProcEquiposEntradasSalidasCon", parametros);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+    }
+}
