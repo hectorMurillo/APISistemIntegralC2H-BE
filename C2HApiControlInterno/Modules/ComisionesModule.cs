2f3c4e36f10c823f1f801215f065574fd8264ca1
ChristianAlvarado <cris_ales@live.com.mx>
2021-08-05 07:51:34 -0600

    Guardado de comisiones

--- C2HApiControlInterno/Modules/ComisionesModule.cs
+++ C2HApiControlInterno/Modules/ComisionesModule.cs
@@ -21,6 +21,35 @@ namespace C2HApiControlInterno.Modules
             Get("/obtenerEmpleadosComisiones", _ => ObtenerEmpleadosConComisiones());
 
             Post("/obtenerComisionesPorEmpleado", _ => ObtenerComisionesPorEmpleado());
+            Post("/guardar-comisiones", _ => GuardarComisionesEmpleado());
+        }
+
+        private object GuardarComisionesEmpleado()
+        {
+            Result<List<ComisionesXEmpleadoModel>> result = new Result<List<ComisionesXEmpleadoModel>>();
+            try
+            {
+                List<ComisionesXEmpleadoModel> lstComisiones = new List<ComisionesXEmpleadoModel>();
+                var parametro = this.BindModel();
+                
+                var comisiones = parametro.data.comisiones;
+
+                foreach (var element in comisiones)
+                {
+                    ComisionesXEmpleadoModel comision = new ComisionesXEmpleadoModel();
+                    comision.Codigo = element.codigo;
+                    comision.Monto = element.monto;
+                    lstComisiones.Add(comision);
+                }
+                int codEmpleado = parametro.data.codEmpleado;
+
+                _DAComisiones.GuardarComisionesEmpleado(lstComisiones, codEmpleado);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+            return Response.AsJson(result);
         }
 
         private object ObtenerComisiones()
--- DA/C2H/DAComisiones.cs
+++ DA/C2H/DAComisiones.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WarmPack.Classes;
 using WarmPack.Database;
+using WarmPack.Extensions;
 
 namespace DA.C2H
 {
@@ -16,6 +17,28 @@ namespace DA.C2H
         public DAComisiones()
         {
             _conexion = new Conexion(ConexionType.MSSQLServer, Globales.ConexionPrincipal);
+        }
+
+        
+
+        public Result<List<ComisionModel>> GuardarComisionesEmpleado(List<ComisionesXEmpleadoModel> comisiones, int codEmpleado)
+        {
+            Result<List<ComisionModel>> result = new Result<List<ComisionModel>>();
+            try
+            {
+                var parametros = new ConexionParameters();
+                parametros.Add("@pCodEmpleado", ConexionDbType.VarChar, codEmpleado);
+                parametros.Add("@pComisiones", ConexionDbType.Xml, comisiones.ToXml("Comisiones"));
+                parametros.Add("@pResultado", ConexionDbType.Bit, System.Data.ParameterDirection.Output);
+                parametros.Add("@pMsg", ConexionDbType.VarChar, System.Data.ParameterDirection.Output, 300);
+
+                result = _conexion.ExecuteWithResults<ComisionModel>("ProcCatComisionesXEmpleadoGuardar", parametros);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+            return result;
         }
 
         public Result<List<ComisionModel>> ObtenerNotasRemision()
