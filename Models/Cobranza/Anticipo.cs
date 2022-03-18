3e7eb9cf39209d5141b4ef8304b1fccc22551590
3duarlalo24 <3duarlalo@gmail.com>
2022-03-18 12:08:18 -0700

    avance anticipos y listas de precios

---
+++ Models/Cobranza/Anticipo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace Models.Cobranza
+{
+    public class Anticipo
+    {
+        public int Codigo { get; set; }
+        public string FechaRegistro { get; set; }
+        public DateTime FechaRegistroDate
+        {
+            get
+            {
+                return FechaRegistro == "" || FechaRegistro == "null/null/null" ? new DateTime(1900, 1, 1) : Convert.ToDateTime(FechaRegistro);
+            }
+        }
+
+        public decimal MontoTotal { get; set; }
+        public int UsuarioRegistro { get; set; }
+        public string NombreCliente { get; set; }
+        public decimal MontoActual { get; set; }
+        public int CodCliente { get; set; }
+        public string CodObras { get; set; }
+        public int CodListaPrec { get; set; }
+        public string NombreListaPrec { get; set; }
+        public int CodVendedor { get; set; }
+        public string NombreVendedor { get; set; }
+        public string Estatus { get; set; }
+    }
+}
---
+++ Models/Cobranza/Obra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Cobranza
+{
+    public class Obra
+    {
+        public int CodObra { get; set; }
+        public string NombreObra { get; set; }
+    }
+}
